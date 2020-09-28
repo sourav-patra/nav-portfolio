57687216ad74f36c1c107c22a7e638a28e043bc9
SouravPatra <sourav.patra@soroco.com>
2020-09-28 23:34:31 +0530

    Deleted career script file

--- script.js
+++ script.js
@@ -38,3 +38,22 @@ menuBars.addEventListener('click', toggleNav);
 for (let i = 0; i < navItems.length; i++) {
   navItems[i].addEventListener('click', toggleNav);
 }
+
+const timelineSwiper = new Swiper('.timeline .swiper-container', {
+  direction: 'vertical',
+  loop: false,
+  speed: 1600,
+  pagination: '.swiper-pagination',
+  paginationBulletRender: (swiper, index, className) => {
+    const year = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-year');
+    return '<span class="' + className + '">' + year + '</span>';
+  },
+  paginationClickable: true,
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+  breakpoints: {
+    768: {
+      direction: 'horizontal',
+    }
+  }
+});
